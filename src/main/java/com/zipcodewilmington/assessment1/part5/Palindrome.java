de478b0aa4a7fbf49494b162af1f155b49996b6b
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-18 14:25:17 -0500

    Completed palindrome and all tests are successful

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,30 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        Integer counter = 0;
+
+        for(int i = 0 ; i <= input.length() ; i++){
+            for(int j = i+1 ; j <= input.length() ; j++){
+                String sub = input.substring(i,j);
+                if(isPalindromic(sub)){
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static boolean isPalindromic(String input) {
+        String rev = BasicStringUtils.reverse(input);
+
+        if(rev.equals(input) == true){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
